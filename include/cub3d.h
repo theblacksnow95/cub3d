478ce52912b874f37822b7dae71a00dc2ac747cf
theblacksnow95 <enrique.murillo24@gmail.com>
2025-10-02 21:01:32 +0200

    feqq

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: emurillo <emurillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/26 13:44:08 by emurillo          #+#    #+#             */
-/*   Updated: 2025/10/02 14:26:43 by emurillo         ###   ########.fr       */
+/*   Updated: 2025/10/02 19:18:34 by emurillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,17 @@
 
 # define RST_ALL          "\x1b[0m"
 # define CLR_RED          "\x1b[31m"
-# define CLR_GRN        "\x1b[32m"
-# define CLR_YLLW       "\x1b[33m"
+# define CLR_GRN          "\x1b[32m"
+# define CLR_YLLW         "\x1b[33m"
 # define CLR_BLUE         "\x1b[34m"
+
+# define NO_ID          "NO"
+# define SO_ID          "SO"
+# define EW_ID          "EW"
+# define EA_ID          "EA"
+# define F_ID           "F"
+# define C_ID           "C"
+
 
 int		check_file_format_n_extention(char *file);
 int		check_data_map(char *file_name);
--- src/parsing/format_check.c
+++ src/parsing/format_check.c
@@ -6,7 +6,7 @@
 /*   By: emurillo <emurillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 13:23:23 by emurillo          #+#    #+#             */
-/*   Updated: 2025/10/02 17:17:22 by emurillo         ###   ########.fr       */
+/*   Updated: 2025/10/02 20:04:11 by emurillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,46 @@ int	check_file_format_n_extention(char *file)
 	return (1);
 }
 
+
+int	ft_isspace(int s)
+{
+	if (!s)
+		return (1);
+	if ((s >= 9 && s <= 13) || s == 32 )
+	{
+		ft_printf("%c\n", s);
+		return (0);
+	}
+	else
+	{
+		ft_printf("Char: %c\n", s);
+		return (1);
+	}
+}
+
+int	check_id(char *line)
+{
+	int		len;
+	// int		id_len;
+
+	while (ft_isspace(*line))
+		(*line)++;
+	len = 0;
+	while (!ft_isspace(*line))
+	{
+		(*line)++;
+		len++;
+	}
+	if (!ft_strncmp(line, NO_ID, len))
+	{
+		ft_printf("Correct _id NO: %s\n", line);
+		ft_printf("Len of ID: #%d\n", len);
+		free(line);
+		exit (1);
+	}
+	return (0);
+}
+
 int	check_data_map(char *file_name)
 {
 	char	*line;
@@ -52,11 +92,16 @@ int	check_data_map(char *file_name)
 	int		fd;
 
 	fd = open(file_name, O_RDWR);
+	if (fd < 0)
+		return (1);
+	params_cnt = 0;
 	while (params_cnt < 6 && (*file_name != '1' || *file_name != '0'))
 	{
 		line = get_next_line(fd);
 		if (!line)
 			return (1);
-		
+		if (!check_id(line))
+			return (1);
 	}
+	return (0);
 }
