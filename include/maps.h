bdecf673b19d1f74288cb7547bee34baffd63979
antuelo <antuel@outlook.com>
2025-11-06 10:00:53 +0100

    creation branch antuel

---
+++ include/maps.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   maps.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antuel <antuel@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/05 14:39:41 by antuel            #+#    #+#             */
+/*   Updated: 2025/11/06 09:48:55 by antuel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAPS_H
+#define MAPS_H
+
+#include "cub3d.h"
+#include "structs.h"
+
+// Archivo: map_parser.c - TÚ puedes implementar:
+
+typedef struct s_map {
+    char	**grid;
+    int		width;
+    int		height;
+    int		player_x;
+    int		player_y;
+    char	player_dir;
+} t_map;
+
+// 1. dLeer y almacenar el grid del mapa
+char	**parse_map_grid(int fd, t_cub *data);
+
+// 2. Validar caracteres del mapa (solo 0,1,N,S,E,W,espacio)
+int		validate_map_chars(char **grid);
+
+// 3. Encontrar posición inicial del jugador
+int		find_player_position(t_map *map);
+
+// 4. Verificar que el mapa esté cerrado por paredes
+int		is_map_closed(char **grid);
+
+#endif
